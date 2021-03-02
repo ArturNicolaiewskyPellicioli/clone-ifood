98686c64a84f3a81f82ae299fbc609fa3ef772a4
Diego Galdino <diegogaldino1345@hotmail.com>
2021-03-02 09:16:03 -0300

    Deatalhe do restaurante e seus produtos

--- src/Context/IfutureProvider.js
+++ src/Context/IfutureProvider.js
@@ -2,10 +2,11 @@ import React, { useState } from "react"
 import IfutureContext from "./IfutureContext"
 
 const IfutureProvider = (props) => {
+    const [restaurantList,setRestaurantList]=useState([])
     const [teste,setTeste]=useState("teste")
 
-    const states = {teste}
-    const setters = {setTeste}
+    const states = {teste,restaurantList}
+    const setters = {setTeste,setRestaurantList}
     const requests = {}
     const data = {states,setters,requests}
 
--- src/routes/Router.js
+++ src/routes/Router.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { BrowserRouter, Route, Switch } from "react-router-dom"
 import Home from '../pages/Home';
 import Login from "../pages/Login"
+import RestaurantDetail from '../pages/RestaurantDetail';
 import Signup from '../pages/Signup';
 
 const Router = () => {
@@ -21,6 +22,9 @@ const Router = () => {
                 </Route>
                 <Route exact path="/home">
                     <Home />
+                </Route>
+                <Route exact path="/restaurant-detail">
+                    <RestaurantDetail />
                 </Route>
             </Switch>
         </BrowserRouter>
