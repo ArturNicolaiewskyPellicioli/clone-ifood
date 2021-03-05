f689ab05f8cc56666f321ca4084283024c05dd3e
ArturNicolaiewskyPellicioli <arturpellicioli@hotmail.com>
2021-03-05 12:45:54 -0300

    carrinho quase quase quase quase la

--- src/pages/Cart/Cart.js
+++ src/pages/Cart/Cart.js
@@ -36,6 +36,10 @@ export const Cart = () => {
   const [carrinho, setCarrinho] = useState("");
 
   const [payment, setPayment] = useState(null);
+  const [confirmButtonStats , setConfirmButtonStats] = useState('disabled')
+  const restaurantDetails = JSON.parse(localStorage.getItem("restaurantDetails"))
+  const carrinhoToVerification = JSON.parse(localStorage.getItem("carrinho"))
+  // console.log(restaurantDetails)
 
   // const {shipping} = resDetail
   //Nome
@@ -44,17 +48,17 @@ export const Cart = () => {
 
   useEffect(() => {
     getFullAddress();
-
-    // getRestaurantDetail(id);
-   
-    // getCart(cart);
-    // getPrice(resDetail,cart)
     setters.setPage('cart')
   }, []);
 
   useEffect(() => {
     setCarrinho(JSON.parse(localStorage.getItem("carrinho")))
-}, [states.cart])
+  }, [states.cart])
+
+  useEffect(() => {
+    cartRestaurantVerification()
+  }, [restaurantDetails])
+
 
   const getAddress = (address) => {
     return (
@@ -67,12 +71,45 @@ export const Cart = () => {
     );
   };
 
+  const cartRestaurantVerification = () => {
+
+    let newCart = []
+  // console.log(carrinhoToVerification)
+  // carrinho ?? carrinho.map((item) => {
+  //   console.log(item.id)
+  // })    
+
+  // console.log(carrinhoToVerification, restaurantDetails)
+  restaurantDetails && restaurantDetails.products.map((prod) => {
+      // let filteredCart = carrinhoToVerification && carrinhoToVerification.filter( item => item.id === prod.id)
+      carrinhoToVerification && carrinhoToVerification.map((item) => {
+        if(item.id ===  prod.id) {
+            // let ultimateFilter = carrinhoToVerification.filter(item => item.id === filteredCart[0].id)
+            // console.log(ultimateFilter)
+          // console.log(filteredCart)
+          newCart.push(item);
+          console.log(newCart)
+        
+          localStorage.setItem('carrinho', JSON.stringify(newCart))
+        }
+      })
+      
+    
+    })
+
+  }
+
+  
+
+  
+  console.log('carrinho', carrinho)
+
   const restaurantInfo = () => {
     return (
       <CardRestaurant>
-        <RedText>{resDetail.name}</RedText>
-        <GrayText>{resDetail.address}</GrayText>
-        <GrayText>{resDetail.deliveryTime} min</GrayText>
+        <RedText>{restaurantDetails.name}</RedText>
+        <GrayText>{restaurantDetails.address}</GrayText>
+        <GrayText>{restaurantDetails.deliveryTime} min</GrayText>
       </CardRestaurant>
     );
   };
@@ -83,6 +120,10 @@ export const Cart = () => {
 
     localStorage.setItem("carrinho", JSON.stringify(newcart))
   }
+
+  useEffect(() => {
+    payment && setConfirmButtonStats('')
+  },[payment])
 
   const getCart = (cart) => {
     const showOrder = cart.map((product) => {
@@ -105,14 +146,14 @@ export const Cart = () => {
       <>
         {restaurantInfo()}
         {showOrder}
-        {orderPrice(resDetail.shipping, carrinho)}
+        {orderPrice(restaurantDetails.shipping, carrinho)}
       </>
     );
   };
 
   const orderPrice = () => {
-    console.log(resDetail.shipping)
-    const ship = resDetail.shipping;
+    // console.log(restaurantDetails.shipping)
+    const ship = restaurantDetails.shipping;
     const showPrice =
       carrinho &&
       carrinho
@@ -172,14 +213,14 @@ export const Cart = () => {
     <CartContainer>
       <p>Meu Carinho</p>
       {address && getAddress(address)}
-      {/* {resDetail && restaurantInfo(resDetail)} */}
+      {/* {restaurantDetails && restaurantInfo(resDetail)} */}
 
       {carrinho.length === 0 ? noCart() : getCart(carrinho)}
       {/* {cart && orderPrice( cart)} */}
 
       {paymentMethod()}
 
-      <ButtonConfirm  onClick={() => createOrder(payment)}>Confirmar</ButtonConfirm>
+      <ButtonConfirm disabled={confirmButtonStats} onClick={() => createOrder(payment)}>Confirmar</ButtonConfirm>
     </CartContainer>
 
 
